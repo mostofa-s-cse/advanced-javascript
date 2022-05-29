ade08c0382a025733d40c346eee77fc89339e662
MOSTOFA_CSE <mostofa.s.cse@gmail.com>
2022-05-29 15:17:33 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,46 @@
+/*const age = 0;
+//const age = 4;
+if (age > 0) {
+    console.log("condition is true");
+
+} else {
+    console.log("condition is false");
+}
+*/
+//...............................................................//
+
+/*
+.................falsy........
+1. 0
+2. ""
+3. undenfined
+4. null
+5. NaN
+6. false
+..............truety...............
+1. '0'
+2. ' '
+3. []
+
+*/
+
+//................................................................//
+/*
+// const name = "";
+// const name = "mortuza";
+
+if (name) {
+    console.log("condition is true");
+
+} else {
+    console.log("condition is false");
+}*/
+//..........................................................................//
+
+let name = 12;
+if (name || name == 0) {
+    console.log("condition is true");
+
+} else {
+    console.log("condition is false");
+}
