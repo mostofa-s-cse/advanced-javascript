b4b6db3b754443aabc4e846a668f14dc31182734
MOSTOFA_CSE <mostofa.s.cse@gmail.com>
2022-05-31 21:33:37 +0600

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,25 @@
+// let pakhi;
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return;
+// }
+
+// function add(num1, num2) {
+//     console.log(num2);
+// }
+// const result = add(13);
+// console.log(result);
+
+
+
+// const premik = { name: "smart boy", phone: 0333343 };
+// console.log(premik.gf);
+
+
+// let fun = undefined;
+// console.log(fun);
+
+
+
+let ages = [2, 3, 4, 5]
+console.log(ages[11]);
