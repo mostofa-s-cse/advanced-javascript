632310b49e5c0fa6b69c1961e84c34c0c03f2dae
MOSTOFA_CSE <mostofa.s.cse@gmail.com>
2022-06-03 19:57:06 +0600

    onek kico sikhlam

---
+++ array-split.js
@@ -0,0 +1,8 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, ];
+// const part = nums.slice(2, 5);
+const removed = nums.slice(2, 4, 77);
+// console.log(part);
+console.log(nums);
+console.log(removed);
+const together = nums.join("ami");
+console.log(together);
---
+++ scope.js
@@ -0,0 +1,17 @@
+let bonus = 20;
+
+function sum(first, second) {
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if (result > 9) {
+        // var mood = "good";
+        const mood = "happy";
+        // console.log(mood);
+    }
+    // console.log(day);
+    // let day = "friday"
+    return result;
+}
+const output = sum(3, 3);
+console.log(bonus);
+console.log(output);
